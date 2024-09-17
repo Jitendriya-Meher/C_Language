d53fbbe9d8051a02da3b64825a1f962a6b2508f2
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-09-18 01:48:09 +0530

    adding lec 71

---
+++ 071 - Pointers in C - part 1/c.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    
+    int a;
+    float b;
+    char c;
+
+    // int *ptra = &b;
+
+    int *ptra = &a;
+    float *ptrb = &b;
+    char *ptrc = &c;
+
+    return 0;
+
+}
