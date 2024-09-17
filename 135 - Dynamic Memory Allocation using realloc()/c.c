a30899cfaeffc96e50eb169da5aa3078d01e7333
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-09-17 23:48:10 +0530

    adding lec 135

---
+++ 135 - Dynamic Memory Allocation using realloc()/c.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+
+    int n,i,*ptr;
+
+    printf("Enter total no. of values : ");
+    scanf("%d",&n);
+
+    ptr = (int*) malloc(n*sizeof(int));
+
+    printf("Enter the values : \n");
+    for(i=0;i<n;i++){
+        printf("Enter value %d : ",i+1);
+        scanf("%d",(ptr+i));
+    }
+
+    printf("values : ");
+    for(i=0;i<n;i++){
+        printf(" %d ",*(ptr+i));
+    }
+
+    printf("\nEnter total no. of values : ");
+    scanf("%d",&n);
+    ptr = (int*) realloc(ptr, n*sizeof(int));
+
+    printf("\nvalues : ");
+    for(i=0;i<n;i++){
+        printf(" %d ",*(ptr+i));
+    }
+
+    free(ptr);
+
+
+    return 0;
+
+}
