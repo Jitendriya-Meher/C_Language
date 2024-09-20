9650824f80185dd9b5cb3bafd05bdcf19f7abd1c
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-09-20 22:13:46 +0530

    adding lec 75

---
+++ 075 - Pointers in C-part 5/c.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+
+    int arr[5] = {1,2,3,4,5};
+    int *p = arr;
+    int *q = &arr[0];
+
+    for( int i = 0; i < 5; i++){
+        printf("%d ",*p);
+        p = p+1;
+    }
+
+    for( int i = 0; i < 5; i++){
+        printf("%d ",*q);
+        q++;
+    }
+
+    return 0;
+
+}
