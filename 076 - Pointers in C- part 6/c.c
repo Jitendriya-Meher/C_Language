46f24b58e261fb10b0d662a7cecc4d54e464455f
Jitendriya-Meher <jiten@gcekbpatna.ac.in>
2024-09-20 22:27:32 +0530

    adding lec 76

--- 076 - Pointers in C- part 6/c.c
+++ 076 - Pointers in C- part 6/c.c
@@ -6,7 +6,21 @@ int main(){
     int *p = arr;
     int *q = &arr[0];
 
-    
+    int *r = &arr[4];
+
+    printf(" %d \n",*r);
+    r-=1;
+    printf(" %d \n",*r);
+
+    r=r-1;
+    printf(" %d \n",*r);
+
+    r--;
+    printf(" %d \n",*r);
+
+    printf(" p-r = %d \n",p-r);
+
+    printf(" r-p = %d \n",r-p);
 
     return 0;
 
